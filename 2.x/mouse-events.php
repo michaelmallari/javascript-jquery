0d76e6f6649c963f5ab0406cd3e5c939233c2d18
Michael Mallari <michael.mallari@nasdaqomx.com>
2014-03-03 15:58:50 -0500

    mouse-events: Works on 1.x and 2.x

---
+++ 2.x/mouse-events.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>fade-onload</title>
+		<link rel="stylesheet" type="text/css" href="mouse-events.css">
+		<script src="/js/jquery-2.1.0.min.js"></script>
+		<script src="mouse-events.js"></script>
+	</head>
+
+
+
+
+
+	<body>
+		<div><br/><strong>Click Me!</strong></div>
+	</body>
+</html>
