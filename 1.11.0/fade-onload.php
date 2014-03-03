a6f3d2c98aae414e296834ac95932eb97cf3dcab
Michael Mallari <michael.mallari@nasdaqomx.com>
2014-03-03 11:36:56 -0500

    fade-onload: fadeOut() works on 1.x and 2.x

---
+++ 1.11.0/fade-onload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>fade-onload</title>
+		<link rel="stylesheet" type="text/css" href="fade-onload.css">
+		<script src="/js/jquery-1.11.0.min.js"></script>
+		<script src="fade-onload.js"></script>
+	</head>
+
+
+
+
+
+	<body>
+		<div id="red"></div>   
+	</body>
+</html>
